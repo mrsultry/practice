1cb232baa8c8f5d05959ab0c80e77a9251d652e0
k1mva <high-b@mail.ru>
2023-10-19 20:56:40 +0500

    armstrong numbers

--- armstrong.py
+++ armstrong.py
@@ -1,5 +1,6 @@
-x = input()
-x = str(x)
+y = int(input())
+'''int(y)
+x = str(y)
 sum_digits = 0
 for digit in x:
     sum_digits = sum_digits + int(digit)**len(x)
@@ -8,8 +9,14 @@ if sum_digits == int(x):
     print("Число Армстронга")
 else:
     print("НЕ является числом Армстронга")
-
-
+'''
+sum_digits = 1
+num_digits=len(str(y))
+print(y//10)
+while (y // 10) > 0:
+    sum_digits += (y % 10)**num_digits
+    y = y // 10
+print(sum_digits)
 
 
 for number in range(10, 10000):
